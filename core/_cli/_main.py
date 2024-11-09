2e2857becab7f91218a49356e435a7a0774e91d5
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2024-11-09 15:20:23 +0200

    Find close intersections

--- core/_cli/_main.py
+++ core/_cli/_main.py
@@ -2,6 +2,7 @@
 import math
 import multiprocessing
 from collections import defaultdict
+from functools import cache
 from pathlib import Path
 
 import ifcopenshell
@@ -25,7 +26,8 @@ pairs = [
     ("IfcColumn", "IfcColumn"),
 ]
 tolerance = 0.002  # TODO: Research the tolerance value
-
+merge_distance = 0.1  # [m]
+filter_dimensions = {"height": 200e-3, "width": 100e-3, "length": 1.5}
 
 # %%
 model = ifcopenshell.open(fpath)
@@ -69,21 +71,23 @@ def _get_material(entity: ifcopenshell.entity_instance) -> str:
     return material
 
 
-def _get_sizes(entity: ifcopenshell.entity_instance):
+def _get_sizes(entity: ifcopenshell.entity_instance) -> dict:
+    if not entity.IsDefinedBy:
+        return {}
+
     sizes = {}
-    if entity.IsDefinedBy:
-        for definition in entity.IsDefinedBy:
-            if definition.is_a("IfcRelDefinesByProperties"):
-                prop_set = definition.RelatingPropertyDefinition
-                if prop_set.is_a("IfcPropertySet"):
-                    for prop in prop_set.HasProperties:
-                        if prop.is_a("IfcPropertySingleValue") and prop.Name in [
-                            "Height",
-                            "Width",
-                            "Length",
-                            "Depth",
-                        ]:
-                            sizes[prop.Name] = prop.NominalValue.wrappedValue
+    for definition in entity.IsDefinedBy:
+        if definition.is_a("IfcRelDefinesByProperties") and (
+            prop_set := definition.RelatingPropertyDefinition
+        ).is_a("IfcPropertySet"):
+            for prop in prop_set.HasProperties:
+                if prop.is_a("IfcPropertySingleValue") and prop.Name in [
+                    "height",
+                    "width",
+                    "length",
+                    "depth",
+                ]:
+                    sizes[prop.Name] = prop.NominalValue.wrappedValue
     return sizes
 
 
@@ -101,9 +105,10 @@ def extract_entity(entity: ifcopenshell.entity_instance) -> dict:
 # TODO: Filter out by LxWxH
 intersections = []
 for clash in clashes:
-    # Mean average of two points
     raw = {}
-    raw["coords"] = [(lhs + rhs) / 2.0 for lhs, rhs in zip(clash.p1, clash.p2)]
+    raw["coords"] = [
+        (lhs + rhs) / 2.0 for lhs, rhs in zip(clash.p1, clash.p2)
+    ]  # mean average
     raw["distance"] = clash.distance
     raw["entities"] = [
         extract_entity(model.by_id(clash.a.id())),
@@ -115,15 +120,36 @@ for clash in clashes:
     intersections.append(raw)
 intersections
 
+
+# %% Merge intersections
+@cache
+def _distance(*args) -> float:
+    return math.sqrt(sum((lhs - rhs) ** 2 for lhs, rhs in zip(args[0:3], args[3:6])))
+
+
+merges = set()
+for i, outer in enumerate(intersections):
+    indices = [i]
+    for j, inner in enumerate(intersections):
+        if i != j and _distance(*outer["coords"], *inner["coords"]) < merge_distance:
+            indices.append(j)
+
+    if len(indices) > 1:
+        merges.add(frozenset(indices))
+merges
+
 # %%
-# TODO: Merge close clashes which fit into the clash_size
-
-
-# %%
-# TODO: Filter intersections out by LxWxH
-
+# TODO: Filter intersections out by LxWxH from filter_dimensions
+# for intersection in intersections:
+#     for entity in intersection["entities"]:
+#         entity["sizes"] = {
+#             key: value
+#             for key, value in entity["sizes"].items()
+#             if key in ["Height", "Width", "Length"]
+#         }
 
 # %% Group items by the combination of elem_a and elem_b materials
+# TODO: STEEL treatment
 grouped = defaultdict(list)
 for intersection in intersections:
     materials = sorted([entity["material"] for entity in intersection["entities"]])
